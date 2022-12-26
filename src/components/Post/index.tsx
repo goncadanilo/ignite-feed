1c7f0788bf36664d8466cf2c71f5ea5b67d5e683
Danilo Gonçalves <danilo.santos@grupoboticario.com.br>
2022-12-26 17:47:19 -0300

    feat: create Post component

---
+++ src/components/Post/index.tsx
@@ -0,0 +1,55 @@
+import { Avatar } from "../Avatar";
+import styles from "./styles.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar
+            src="https://github.com/goncadanilo.png"
+            alt="Danilo Gonçalves"
+          />
+
+          <div className={styles.authorInfo}>
+            <strong>Danilo Gonçalves</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="26 de Dezembro às 15:30h" dateTime="2022-12-26 15:30:00">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+
+        <p>
+          <a href="#">jane.design/doctorcare</a>
+        </p>
+
+        <p>
+          <a href="#">#novoprojeto</a> &nbsp;
+          <a href="#">#nlw</a> &nbsp;
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+    </article>
+  );
+}
